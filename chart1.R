b6bf2b4024b1327c1bdc5505496098a29710f5d8
lex <alexisnguyen@Alexiss-MacBook-Air.local>
2022-11-18 14:47:39 -0800

    update

---
+++ chart1.R
@@ -0,0 +1,22 @@
+library(ggplot2)
+hist_2017 <- Data.Table.1$X.2
+hist_2018 <- Data.Table.1$X.3
+hist_2019 <- Data.Table.1$X.4
+hist_2020 <- Data.Table.1$X.5
+hist_2021 <- Data.Table.1$X.6
+
+     
+plot(hist_2021, col = 'red', xlim = c(4,17),
+     ylim = c(5,150), main = "Gun Violence over the years (2017-2021) from police", 
+     xlab = "Months: Jan = 5 to Dec = 16",
+     ylab = "Number of victims from gun violence")
+
+points(hist_2017, col = 'blue', lwd = 5)
+points(hist_2018, col = 'purple', lwd = 5)
+points(hist_2019, col = 'orange', lwd = 5)
+points(hist_2020, col = 'green', lwd = 5)
+points(hist_2021, col = 'red', lwd = 5)
+legend(4, 125, legend = c("2017", "2018", "2019", "2020", "2021"), 
+       col = c('blue', 'purple', 'orange', 'green', 'red'), lty = 1:2,
+       cex = 0.8)
+
---
+++ chart2.R
@@ -0,0 +1,7 @@
+month_numbers <- (x=c(525, 516, 554, 493, 518, 530, 499, 501, 
+                      424, 446, 403, 435))
+months <- c("Jan", "Feb", "March", "April", "May", "June", "July",
+            "Aug", "Sept", "Oct", "Nov", "Dec")
+barplot(month_numbers, names.arg = months, main = "Victims to Gun-Violence by Month from 2017-2022 from police",
+       col = 'pink', xlab = "Months", ylab = "# of deaths", ylim=c(0,600))
+
