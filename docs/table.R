21f5f47c305b8492fe973941f9b8805b654c42fc
Greta Carsten <gretacarsten@MacBook-Air-10.localdomain>
2022-11-17 23:14:26 -0800

    table

--- docs/table.R
+++ docs/table.R
@@ -1,30 +1,22 @@
-left_join()
-remove()
+fatal_police_shootings <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv")
+View(fatal_police_shootings)
 
-dataset_3 <- read.csv("https://www.statista.com/statistics/585152/people-shot-to-death-by-us-police-by-race/")
-View(dataset_3)
-
-df1 <- fatal.police.shootings.death 
-
-table_of_shootings <- table(df1$fatal.police.shootings.death, df)
-
-shootings <- read.csv("https://github.com/washingtonpost/data-police-shootings/blob/master/fatal-police-shootings-data.csv")
-View(shootings)
-
-fatal.police.shootings <- read.csv("https://raw.githubusercontent.com/washingtonpost/data-police-shootings/master/fatal-police-shootings-data.csv")
-View(fatal.police.shootings)
-
-data.frame1 <- fatal.police.shootings
-
-table_of_shootings <- table(df$fatal.police.shootings, )
+dataframe1 <- fatal_police_shootings
 
 people_shot_by_race <- read.csv("https://raw.githubusercontent.com/info201b-au2022/project-gretacarsten18/main/data/statistic_id585152_people-shot-to-death-by-us-police-2017-2022-by-race.csv")
 View(people_shot_by_race)
 
+dataframe2 <- people_shot_by_race
+
 people_shot_by_month <- read.csv ("https://raw.githubusercontent.com/info201b-au2022/project-gretacarsten18/main/data/statistic_id585159_people-shot-to-death-by-us-police-2017-2022-by-month%20(2).%20data%20set%201.csv")
 View(people_shot_by_month)
 
-df1 <- shootings
+dataframe3 <- people_shot_by_month
 
-table_of_police_brutality <- table(df$fatal.police.shootingsdf$people_shot_by_race, df$people_shot_by_month)
+t(people_shot_by_race)
+View(people_shot_by_race)
+
+table_of_police_brutality <- left_join(dataframe2$row_variable, dataframe3$column_variable)
 View(table_of_police_brutality)
+
+
